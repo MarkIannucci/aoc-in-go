57d54fb1fe67ac24fe14d94352c9156ee175473d
Mark Iannucci <mark.iannucci@gmail.com>
2024-12-03 07:43:31 +0000

    Day two part one is done

--- 2024/02/code.go
+++ 2024/02/code.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"strings"
 	"slices"
-	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
@@ -18,9 +19,7 @@ func main() {
 // 4. with: true (part2), and user input
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
-	// Process the input into two sorted arrays
 	lines := strings.Split(input, "\n")
-
 	var safeLines int
 	safeLines = 0
 	for _, line := range lines {
@@ -32,37 +31,45 @@ func run(part2 bool, input string) any {
 			if value, err := strconv.Atoi(part); err == nil {
 				partsInt[i] = value
 			}
-		
+		}
+
 		sorted := make([]int, len(partsInt))
 		copy(sorted, partsInt)
 		slices.Sort(sorted)
 
-		if slices.Equal(sorted, parts) {
+		if slices.Equal(sorted, partsInt) {
 			// do nothing because we need to continue evaluating if it is potentially safe
 		} else {
 			slices.Reverse(sorted)
-			if slices.Equal(sorted, parts) {
+			if slices.Equal(sorted, partsInt) {
 				slices.Sort(sorted)
 			} else {
-				break
+				continue
 			}
 		}
 
-		// check if each number in the sorted string is increasing by at least 1 and not more than 3	
-		for i := 0; i < len(sorted) - 1; i++ {
-			if sorted[i + 1] - sorted[i] > 3 {
-				isSafe = false
-				break
+		// check if each number in the sorted string is increasing by at least 1 and not more than 3
+		var safe bool
+		safe = true
+		for i := 0; i < len(sorted)-1; i++ {
+			if sorted[i+1]-sorted[i] == 0 {
+				safe = false
 			}
+			if sorted[i+1]-sorted[i] >= 4 {
+				safe = false
+			}
+		}
+		if safe {
+			safeLines = safeLines + 1
+		} else {
 		}
 
 	}
 
-	
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
 		return "not implemented"
 	}
 	// solve part 1 here
-	return 43
+	return safeLines
 }
